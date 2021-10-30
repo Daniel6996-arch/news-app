b79bf7c8c3f829fbe99516142611501695380bad
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-30 08:46:10 +0300

    add debug edits to produce working template

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask._bootstrap import Bootstrap
+#from flask._bootstrap import Bootstrap
 from config import config_options
 
 bootstrap = Bootstrap()
@@ -11,7 +11,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
-    bootstrap.init_app(app)
+   # bootstrap.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint

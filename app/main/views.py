6a4a18404044ef6318d69da5763ac301ad189fb9
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-10-31 13:31:19 +0300

    adding sources to views

--- app/main/views.py
+++ app/main/views.py
@@ -3,19 +3,20 @@ from . import main
 from ..requests import get_news
 from ..models import News
 
-
 @main.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-
+  
     # Getting news articles
-    tesla_news = get_news('tesla')
-    apple_news = get_news('apple')
+    tesla_news = get_news('everything','q','tesla')
+    apple_news = get_news('everything','q','apple')
+    us_news = get_news('top-headlines','country','us')
+    techcrunch_news = get_news('everything','sources','techcrunch')
     title = 'Home - Welcome to The best News resource Online'
-    return render_template('news.html', title = title, tesla = tesla_news, apple = apple_news )
+    return render_template('news.html', title = title, tesla = tesla_news, apple = apple_news, us = us_news, techcrunch = techcrunch_news )
 
 @main.route('/sources')
 def sources():
@@ -29,6 +30,5 @@ def sources():
     apple_news = get_news('apple')
     country = get_news('us')
     sources = get_news('techcrunch')
-    domains = get_news('wsj.com')
     title = 'Sources - Welcome to The best News resource Online'
-    return render_template('sources.html', title = title, tesla = tesla_news, apple = apple_news, us = country, techcrunch = sources, wsj.com = domains )
+    return render_template('sources.html', title = title, tesla = tesla_news, apple = apple_news, us = country, techcrunch = sources )
--- app/requests.py
+++ app/requests.py
@@ -15,11 +15,11 @@ def configure_request(app):
     base_url = app.config['NEWS_API_BASE_URL']
 
 
-def get_news(q):
+def get_news(source,q,tesla):
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(q,api_key)
+    get_news_url = base_url.format(source,q,tesla,api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
